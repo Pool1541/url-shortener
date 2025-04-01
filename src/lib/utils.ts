3582d433e183238aa655fd4b2683e6c859b91fb0
pool_1541 <pool_1541@hotmail.com>
2025-04-01 00:25:12 -0500

    chore: add utility function to validate URLs

---
+++ src/lib/utils.ts
@@ -0,0 +1,10 @@
+export function isValidUrl(url: string) {
+  try {
+    const parsedUrl = new URL(url);
+    return parsedUrl.protocol === 'http:' || parsedUrl.protocol === 'https:';
+  } catch (error) {
+    if (error instanceof Error) return false;
+
+    return false;
+  }
+}
