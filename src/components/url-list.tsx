7b853b8604918215747f6f5a449c829dbc1065b7
pool_1541 <pool_1541@hotmail.com>
2025-04-06 01:04:25 -0500

    style: adjust overflow properties for URL list container

--- src/components/url-list.tsx
+++ src/components/url-list.tsx
@@ -5,7 +5,7 @@ export default function UrlList() {
   return (
     <UrlsContainer>
       {({ isError, isLoading, shortenedUrls, refetch }) => (
-        <div className='w-full max-w-4xl mt-8 px-4 max-h-[500px] overflow-auto scrollbar-thin scrollbar-indigo'>
+        <div className='w-full max-w-4xl mt-8 px-4 max-h-[500px] overflow-x-hidden overflow-y-auto scrollbar-thin scrollbar-indigo'>
           {isLoading && <p>Loading...</p>}
           {isError && <p>Error loading URLs</p>}
           {shortenedUrls && shortenedUrls.urls.map((url) => <UrlItem key={url.shortUrl} url={url} refetch={refetch} />)}
