52623ee3b264fa7ca3bd833cd42d96e807e5b3ad
Pool Llerena <pool_1541@hotmail.com>
2025-04-09 11:15:15 -0500

    refactor: remove StrictMode wrapper from the root render

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react';
+
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'sonner';
@@ -16,10 +16,8 @@ const queryClient = new QueryClient({
 });
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Router />
-      <Toaster richColors expand={false} position="top-right" />
-    </QueryClientProvider>
-  </StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <Router />
+    <Toaster richColors expand={false} position="top-right" />
+  </QueryClientProvider>
 );
